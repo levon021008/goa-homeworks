91336c6cb1eef1654a70e3683f1ce8ef9f470cfc
levon021008 <levonstanoff@gmail.com>
2024-07-08 18:27:29 +0400

    first commit

---
+++ day 3/day 3.py
@@ -0,0 +1,8 @@
+a=input(input("enter number one:"))
+b=input(input("enter number second:"))
+
+print(a-b)
+print(a+b)
+
+
+
---
+++ day 5/homeworkn2.py
@@ -0,0 +1,7 @@
+name=("input your name: ")
+surname=("input your surname: ")
+age=("input your age: ")
+city=("input your city: ")
+
+
+print("Levon Melikyan 16 clis vcxovrob kalak Tbilisshi")
