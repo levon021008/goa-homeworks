8f3d79b967da27140192a0f88f485fd35f7963a8
levon021008 <levonstanoff@gmail.com>
2024-07-23 22:31:33 +0400

    first commit

--- day 7/homework.py
+++ day 7/homework.py
@@ -1,8 +1,5 @@
-name = ("wgat is your name?")
-user_age = int(input("how old are you?"))
-user_age = int("memelikyan")
-club_list = ["levon,dima,goga,gio"]
-user_age >= 18
-club_list.append(name)
-print("hello gow mane time you want to travel in future")
-print("many times because i want to see everything what is happen in futute")
+name = ("what is your name?")
+user_age = int(input("how old are you"))
+int(input("how many time you want to travel?"))
+int(input("many"))
+int(input("how old are you"))
