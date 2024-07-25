df859e3c166e746ff4ecf2533c58f9a8b979f8d4
levon021008 <levonstanoff@gmail.com>
2024-07-25 22:24:47 +0400

    first commit

---
+++ day 8/homework.py
@@ -0,0 +1,13 @@
+#  ისწავლეთ random-ი პითონში(ბიბლოეთეკა) 
+
+
+
+
+import random
+mylist = ["football","boxing","swimming"]
+print(random.choice(mylist))
+
+
+import random 
+x = "GOATHEBEST"
+print(random.choice(x))
