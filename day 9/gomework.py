b840b5ce31f00060146949ff0decc8a746eaaa45
levon021002 <levonstanoff@gmail.com>
2024-07-30 20:22:11 +0400

    first commit

---
+++ day 9/gomework.py
@@ -0,0 +1,7 @@
+#შექმენით პროგრამა რომელსაც შეეძლება დიალოგი თქვენთან (აირჩიეთ ნებისმიერი თემა) input გამოყენებით
+
+print("what is your name")
+a=input("levon")
+print("how old are you?")
+b=int(input("16"))
+
